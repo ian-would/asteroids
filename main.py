ced1c224366c7fdf19d937919df319f8d0a52c8e
ian_would <ianwood0225@gmail.com>
2025-01-03 09:10:40 -0800

    Draw player update

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    user = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        user.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
