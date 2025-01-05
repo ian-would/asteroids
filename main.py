eec49c7d8b9f4000fcfa836ab1d44fcddd625a3a
ian_would <ianwood0225@gmail.com>
2025-01-05 08:54:52 -0800

    Asteroid splitting

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
 
             for shot in shots:
                 if asteroid.collisions(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
          
         screen.fill("black")
